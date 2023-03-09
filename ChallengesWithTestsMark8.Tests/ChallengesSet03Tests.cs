24996de061b3cf737fc9dd4619a4a4099165857f
Faith Goins <faithtoriangoins@gmail.com>
2023-03-09 17:48:01 -0600

    fixed error challenge set 3

--- ChallengesWithTestsMark8.Tests/ChallengesSet03Tests.cs
+++ ChallengesWithTestsMark8.Tests/ChallengesSet03Tests.cs
@@ -200,7 +200,7 @@ namespace ChallengesWithTestsMark8.Tests
             };
 
             // Act
-            int[] actual = challenger.GetOddsBelow100();
+            int[] actual = challenger.GetOddsBelow100(odds);
 
             // Assert
             Assert.Equal(odds, actual);
